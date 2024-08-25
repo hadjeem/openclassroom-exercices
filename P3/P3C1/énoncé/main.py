4b6d5441b63bdbe987ce577d20073e9b20d4403e
hadje <hadjera.emroska@student.vinci.be>
2024-08-26 01:23:29 +0200

    ex9

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,5 +1,11 @@
 # Ecrivez votre code ici !
+import operations
 
+def main():
+    addition = operations.addition(3,5)
+    multiplication = operations.multiplication(8,2)
+    print("Résultat de l'addition :", addition)
+    print("Résultat de la multiplication :", multiplication)
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
