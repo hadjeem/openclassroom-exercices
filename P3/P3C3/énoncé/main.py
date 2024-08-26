371edb8e5ba749289ba4283c2e0fab23be265403
hadje <hadjera.emroska@student.vinci.be>
2024-08-26 17:54:58 +0200

    ex11

--- P3/P3C3/énoncé/main.py
+++ P3/P3C3/énoncé/main.py
@@ -1,4 +1,36 @@
+import csv
 # Écrivez votre code ici !
+
+def extract():
+    data = []
+    with open("input.csv") as file:
+        reader = csv.DictReader(file, delimiter=",")
+        for line in reader:
+            data.append(line)
+    return data
+
+def transform(data):
+    data_to_load = []
+    for line in data:
+        transformed_data = {}
+        transformed_data["nom"] = line["nom"]
+        transformed_data["salaire"] = int(line["heures_travaillees"]) * 15
+        data_to_load.append(transformed_data)
+    return data_to_load    
+
+def load(data):
+    with open("output.csv", "w", newline="") as file:
+        fieldnames = ["nom", "salaire"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for line in data:
+            writer.writerow(line)
+
+def main():
+    data = extract()
+    data_to_load = transform(data)
+    load(data_to_load)
+
 
 
 # Ne touchez pas le code ci-dessous
